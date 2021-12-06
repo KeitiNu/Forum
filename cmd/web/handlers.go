d998eec146e97b49b63c9cb8948fb56e34a2db11
roosarula <roosarula@gmail.com>
2021-12-06 18:04:13 +0200

    Added 404 page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	// If a user enters a address that doesn't exist display 404 Error!
+	if r.URL.Path != "/" {
+		app.render(w, r, "400.page.tmpl", nil)
+		return
+	}
 	app.render(w, r, "home.page.tmpl", nil)
 }
