b5f4420479e40c6a9311ffbbaf74e874bcd8ba89
nimi25820 <robert.laur.private@gmail.com>
2022-01-19 15:14:21 +0200

    Docker fixes

--- dockerremove.sh
+++ dockerremove.sh
@@ -1,14 +1,11 @@
 #Stops the container called forumcontainer.
 docker stop forumcontainer 
 
-#Deletes the container called forumcontainer.
-docker rm forumcontainer 
+#Deletes all the containers not running
+docker container prune
 
-#Deletes the image called forumimage.
-docker image rm forumimage
-
-#Deletes the BaseImage.
-docker image rm golang:1.17-buster 
+#Deletes all the images not used by a container
+docker image prune -a
 
 #Shows you that all images are gone
 docker images
