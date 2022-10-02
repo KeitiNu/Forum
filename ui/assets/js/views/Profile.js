495294717b83eb0046cc455fe52109f16f407c83
KeitiN <keiti.nurmbek@gmail.com>
2022-10-02 23:25:04 +0300

    fixed chat problem

--- ui/assets/js/views/Profile.js
+++ ui/assets/js/views/Profile.js
@@ -18,6 +18,7 @@ export default class extends AbstractView {
 			<div class="accounth2">
 				<h2>Account details</h2>
 			</div>
+
 			<div class="accountdetails">
 				<div class="profilename">
 					<h3>Username</h3>
@@ -26,59 +27,7 @@ export default class extends AbstractView {
 				<div class="profileemail">
 					<h3>Email</h3>
 					<a>${this.params.AuthenticatedUser.Email}</a>
-				</div>		
-			</div>
-
-
-			<div class="activity">
-				<div class="accounth2">
-					<h2>Your activity</h2>
 				</div>
-
-				<div class="">
-					<a href="/profile/posts"><button type="button" class="btn submitbtn" data-link>Posts</button></a>
-					<a href="/profile/comments"><button type="button" class="btn submitbtn" data-link>Comments</button></a>
-				</div>
-
-				<div class="mainboxinside">
-					<div class="categories">
-						<div class="insidecategories">
-							{{range .Posts}}
-							<div class="insidecatepadding">
-								<div class="catecard">
-									<div class="card-body">
-										<div class="post-card" id="post{{.ID}}">
-												<div class="postdetails">
-												<div class="post-username">Posted by {{.User}} {{timeAgo .Created}}</div>
-												<div class="post-title"><a class="post-title stretched-link" href="/post/{{.ID}}" data-link>{{.Title}}</a></div>
-												<div class="post-description">{{.Content}}</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>	
-							{{end}}
-
-							{{range .Comments}}
-							<div class="insidecatepadding">
-								<div class="catecard">
-									<div class="card-body">
-										<div class="postcard">
-											<div class="votingbuttons">
-											</div>
-											<div class="postdetails">
-												<div class="post-username">Posted by {{.User}} {{timeAgo .Created}}</div>
-												<div class="post-description-thread">{{.Content}}</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							{{end}}
-						</div>
-					</div>
-				</div>
-				
 			</div>
 		</div>
 			</div>
